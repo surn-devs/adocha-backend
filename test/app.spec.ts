b40f4897ad269084adacd429de5ca2e95bffd753
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-07-13 18:56:49 +0200

    Configure E2E tests

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('AppController', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,7 +15,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('GET /', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
